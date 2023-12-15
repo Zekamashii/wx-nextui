b16c6027e0e41ca46450e2969e500b8c996a8587
Yuhao <yuhaofeng@businessclinic.tokyo>
2023-12-15 12:36:09 +0900

    minor adjustments

--- components/Header.js
+++ components/Header.js
@@ -8,7 +8,7 @@ export default function Header() {
     <div className='flex flex-col items-center justify-center'>
       <h1 className='text-center'>HAPPY EARTH DAY</h1>
       <h3 className='text-center'>You could fry an egg on the sidewalk. Have you seen the weather? Find out below!</h3>
-      <Image src={profilePic} width={200} height={200} alt='B-52 bombers' />
+      <Image src={profilePic} width={200} height={200} alt='B-52 bombers' priority={true} />
       <Divider className='my-4 max-w-4xl' />
     </div>
   );
