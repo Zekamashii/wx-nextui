b9748de239e506d7539aaef49b2cbfff78467a3a
Yuhao <yuhaofeng@businessclinic.tokyo>
2023-12-15 12:19:22 +0900

    minor adjustments

--- components/InputBox.js
+++ components/InputBox.js
@@ -103,9 +103,9 @@ export default function InputBox() {
           <Card isFooterBlurred radius='lg' className='border-none'>
             <Image src={profilePic} className='object-cover' height={300} width={300} alt='How dare you!' />
             <CardFooter className='justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10'>
-              <p className='text-tiny text-white/80 text-left'>Region: {data?.nearest_area[0].region[0].value}</p>
+              <p className='text-tiny text-white/80 text-left'>{data?.nearest_area[0].region[0].value}</p>
               <Button className='text-tiny text-white bg-black/20' variant='flat' color='default' radius='lg' size='sm'>
-                Country: {data?.nearest_area[0].country[0].value}
+                {data?.nearest_area[0].country[0].value}
               </Button>
             </CardFooter>
           </Card>
